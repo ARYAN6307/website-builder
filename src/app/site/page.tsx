9bd03bca36208a29d1e3b652419ed0257685ee99
Aryan Pandey <aryan.lko04@gmail.com>
2024-05-16 20:24:57 +0530

    first

---
+++ src/app/site/page.tsx
@@ -0,0 +1,23 @@
+
+
+export default function Home(){
+    return (
+<>
+    <section className="h-full w-full pt-36 relative flex items-center justify-center flex-col ">
+     
+     
+     <div className="absolute bottom-0 left-0 right-0 top-0  dark:bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"/>
+        <p className="text-center">Run your agency, in one place</p>
+        <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
+            <h1 className="text-9xl font-bold text-center md:text-[300px]">
+                Webbie
+            </h1>
+        </div>
+    <div className="flex justify-center items-center relative md:mt-[-70px]">
+        
+    </div>
+
+    </section>
+</>
+    )
+}
---
+++ src/middleware.ts
@@ -0,0 +1,55 @@
+import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/dist/server/web/spec-extension/response";
+ 
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
+export default clerkMiddleware({
+    publicRoutes: ['/','/site', '/api/uploadthing'],
+    async beforeAuth(auth: any, req: any) {},
+  async afterAuth(auth: any, req: { nextUrl: any; headers: any; url: string | URL | undefined; }) {
+    //rewrite for domains
+    const url = req.nextUrl
+    const searchParams = url.searchParams.toString()
+    let hostname = req.headers
+
+    const pathWithSearchParams = `${url.pathname}${
+      searchParams.length > 0 ? `?${searchParams}` : ''
+    }`
+
+    //if subdomain exists
+    const customSubDomain = hostname
+      .get('host')
+      ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
+      .filter(Boolean)[0]
+
+    if (customSubDomain) {
+      return NextResponse.rewrite(
+        new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
+      )
+    }
+
+    if (url.pathname === '/sign-in' || url.pathname === '/sign-up') {
+      return NextResponse.redirect(new URL(`/agency/sign-in`, req.url))
+    }
+
+    if (
+      url.pathname === '/' ||
+      (url.pathname === '/site' && url.host === process.env.NEXT_PUBLIC_DOMAIN)
+    ) {
+      return NextResponse.rewrite(new URL('/site', req.url))
+    }
+
+    if (
+      url.pathname.startsWith('/agency') ||
+      url.pathname.startsWith('/subaccount')
+    ) {
+      return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url))
+    }
+  },
+});
+ 
+export const config = {
+      matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
+ 
